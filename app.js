3257a59bbe086e6c728f548258ac147f439fd391
chaitanya <dimpuchaithu@gmail.com>
2025-07-13 11:45:25 +0530

    changed port to 3000 and added 0.0.0.0

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+  res.end('I lob lobb lobbu Rachuuu\n');
 });
 
 const PORT = 3000;
