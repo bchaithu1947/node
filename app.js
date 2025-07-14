e68435cf6f1a17d0ee4ec6c3edb5e7666e062aa6
chaitanya <dimpuchaithu@gmail.com>
2025-07-14 12:41:31 +0530

    time2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('The time is 12:36.\n');
+  res.end('hello world.\n');
 });
 
 const PORT = 3000;
